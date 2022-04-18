08eb7fe2c814b56dce79243bb2ea50556bce15c7
Roberto Vieira <roberto.vieira@outlook.com>
2022-04-18 19:22:56 -0300

    First commit

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+import validator from 'validator';
+
+let email: string = "teste@gmai.c"
+console.log( validator.isEmail(email));
+
+
+
+
+
+
+
+
+
+
+
+// import Matematica from './matematica';
+// import { somar, subtrair } from './matematica';
+// import * as Matematica from './matematica';
+// const Matematica = require('./matematica');
+
+// let n1: number = 10;
+// let n2: number = 2;
+
+// console.log(`SOMA: ${Matematica.somar(n1, n2)}`);
+// console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`);
+// console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`);
